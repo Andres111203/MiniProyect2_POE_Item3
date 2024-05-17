08e10abaf27d7932250fbbf7afd4c6e2b0ee6f1c
Juan_F <juan.e.franco@correounivalle.edu.co>
2024-05-17 12:57:21 -0500

    se crea la barra de menú

--- VSCWindow/src/VSCWindow.java
+++ VSCWindow/src/VSCWindow.java
@@ -10,6 +10,14 @@ public class VSCWindow extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         
+        JMenuBar menuBar = new JMenuBar();
+        JMenu fileMenu = new JMenu("File");
+        JMenuItem openFolderItem = new JMenuItem("Open Folder...");
+        JMenuItem openFileItem = new JMenuItem("Open File...");
+        fileMenu.add(openFolderItem);
+        fileMenu.add(openFileItem);
+        menuBar.add(fileMenu);
+        setJMenuBar(menuBar);
         
     
     public static void main(String[] args) {
